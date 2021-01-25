fc8e43e2deca67609316f590fd6afcd47102d3ef
wolfdate25 <wolfdate25@naver.com>
2021-01-25 22:12:15 +0900

    add bootstrap module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Nav/HeaderNav"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {/* 상단 네비게이션 바*/}
+    <Header />
+    {/* 중앙 페이지 */}
+    {/* 하단 네비게이션 바*/}
     </div>
   );
 }
