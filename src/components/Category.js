3870552aae52b2327b2d9b1440b625c82961ae1c
wolfdate25 <wolfdate25@naver.com>
2021-01-31 22:00:15 +0900

    List items by category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,21 @@
 import React from "react";
+import {organizations} from "./Products";
+
+
+function ListItems({type}) {
+    return (
+        organizations.map((company,index) => (
+            company.products.filter(product =>
+            product.category.includes(type)
+            ).map((product,index2) => <div key={(index*100)+index2}>{product.productName}</div>)
+
+        ))
+    );
+}
 
 export default function Category({category}) {
 
     return (
-        <div>{category}</div>
-    )
+        <ListItems type={category}/>
+    );
 }
