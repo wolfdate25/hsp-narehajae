ace9a7831f419a1861cf2aab80ee54d1b5239417
yuseon-Lim <lys4486@gmail.com>
2021-02-01 21:18:06 +0900

    add category page

---
+++ src/components/Shop.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Shop.css";
+
+function Shop({
+  id,
+  image,
+  productName,
+  companyName,
+  contact,
+  email,
+  url,
+  summary,
+  description,
+}) {
+  return (
+    <div className="product">
+      <div className="product__data">
+        <img src={image} alt={productName} title={productName} />
+        <h3 className="product__productName">{productName}</h3>
+        <h4 className="product__companyName">{companyName}</h4>
+        <h5 className="product__contact">Contact: {contact}</h5>
+        <h5 className="product__email">Email: {email}</h5>
+        <h5 className="product__url">URL: {url}</h5>
+        <p className="product__summary">{summary}</p>
+        <p className="proudct__description">{description.slice(0, 150)}...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Shop;
