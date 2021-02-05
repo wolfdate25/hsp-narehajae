bffdcbf5e51ec49362f476dde5dbd1dd21454ff9
wolfdate25 <wolfdate25@naver.com>
2021-02-05 12:52:47 +0900

    reformat with prettier

--- src/components/Nav/LeftPanel.js
+++ src/components/Nav/LeftPanel.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import {LeftPanelData} from './LeftPanelData';
-import SubMenu from './SubMenu';
-import {IconContext} from 'react-icons/lib';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { LeftPanelData } from "./LeftPanelData";
+import SubMenu from "./SubMenu";
+import { IconContext } from "react-icons/lib";
 
 const Nav = styled.div`
   background: #a0d911;
@@ -32,7 +32,7 @@ const SidebarNav = styled.nav`
   justify-content: center;
   position: fixed;
   top: 0;
-  left: ${({sidebar}) => (sidebar ? '0' : '-100%')};
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
   transition: 350ms;
   z-index: 10;
 `;
@@ -42,40 +42,42 @@ const SidebarWrap = styled.div`
 `;
 
 const useSlider = () => {
-    const [sidebar, setSidebar] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
 
-    return {
-        sidebarStatus: sidebar,
-        setSidebar: setSidebar
-    }
-}
+  return {
+    sidebarStatus: sidebar,
+    setSidebar: setSidebar,
+  };
+};
 
 const LeftPanel = () => {
-    const {sidebarStatus, setSidebar} = useSlider();
+  const { sidebarStatus, setSidebar } = useSlider();
 
-    const toggleSidebar = () => setSidebar(!sidebarStatus);
+  const toggleSidebar = () => setSidebar(!sidebarStatus);
 
-    return (
-        <>
-            <IconContext.Provider value={{color: '#fff'}}>
-                <Nav>
-                    <NavIcon to='#'>
-                        <FaIcons.FaBars onClick={toggleSidebar}/>
-                    </NavIcon>
-                </Nav>
-                <SidebarNav sidebar={sidebarStatus}>
-                    <SidebarWrap>
-                        <NavIcon to='#'>
-                            <AiIcons.AiOutlineClose onClick={toggleSidebar}/>
-                        </NavIcon>
-                        {LeftPanelData.map((item, index) => {
-                            return <SubMenu item={item} key={index} toggleEvent={toggleSidebar}/>;
-                        })}
-                    </SidebarWrap>
-                </SidebarNav>
-            </IconContext.Provider>
-        </>
-    );
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavIcon to="#">
+            <FaIcons.FaBars onClick={toggleSidebar} />
+          </NavIcon>
+        </Nav>
+        <SidebarNav sidebar={sidebarStatus}>
+          <SidebarWrap>
+            <NavIcon to="#">
+              <AiIcons.AiOutlineClose onClick={toggleSidebar} />
+            </NavIcon>
+            {LeftPanelData.map((item, index) => {
+              return (
+                <SubMenu item={item} key={index} toggleEvent={toggleSidebar} />
+              );
+            })}
+          </SidebarWrap>
+        </SidebarNav>
+      </IconContext.Provider>
+    </>
+  );
 };
 
 export default LeftPanel;
