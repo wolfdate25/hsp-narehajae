390af8e43be4b7686ead008bebd9f43a1540fa0f
yuseon-Lim <lys4486@gmail.com>
2021-02-06 21:50:10 +0900

    add Detail Category page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Category from "./components/Category";
+import Category from "./components/Cetegory/Category";
+import DetailCategory from "./components/Cetegory/DetailCategory";
 import Home from "./components/Home";
 import React from "react";
 import LeftPanel from "./components/Nav/LeftPanel";
@@ -12,8 +13,8 @@ function App() {
       <LeftPanel />
       <Switch>
         <Route path="/home" render={(props) => <Home {...props} />} />
-        <Route path="/category/:category" component={Category}></Route>
-        <Route path="/category" component={Category} /> 
+        <Route path="/category/:category" component={DetailCategory} />
+        <Route path="/category" component={Category} />
         {/*라우팅*/}
         {/*<Route*/}
         {/*    path="/other"*/}
---
+++ src/components/Cetegory/DetailCategory.js
@@ -0,0 +1,104 @@
+import React from "react";
+import Shop from "./Shop.js";
+import { Product } from "../Products.js";
+
+class DetailCategory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ products: Product });
+  }
+
+  render() {
+    const { products } = this.state;
+    const category = this.props.match.params.category;
+    return (
+      <div>
+        {products
+          ? Object.keys(products).map((productKey) => {
+              if (productKey === category) {
+                return (
+                  <div className="products" key={productKey}>
+                    {Object.values(products[productKey]).map((product, id) => {
+                      console.log(product);
+                      return (
+                        <Shop
+                          key={product.id}
+                          image={product.image}
+                          productName={product.productName}
+                          companyName={product.companyName}
+                          contact={product.contact}
+                          email={product.email}
+                          url={product.url}
+                          summary={product.summary}
+                          description={product.description}
+                        />
+                      );
+                    })}
+                  </div>
+                );
+              }
+            })
+          : "no products"}
+      </div>
+    );
+  }
+}
+
+export default DetailCategory;
+
+/* import React from "react";
+import {organizations} from "./Products";
+
+
+function ListItems({type}) {
+    return (
+        organizations.map((company,index) => (
+            company.products.filter(product =>
+            product.category.includes(type)
+            ).map((product,index2) => <div key={(index*100)+index2}>{product.productName}</div>)
+
+        ))
+    );
+}
+
+export default function Category({category}) {
+
+    return (
+        <ListItems type={category}/>
+    );
+}
+*/
+
+/*
+import React from "react";
+import {organizations} from "./Products";
+
+
+function ListItems({type}) {
+    return (
+        organizations.map((company, index) => (
+            company.products.filter(product =>
+                product.category.includes(type)
+            ).map((product, index2) => <div key={(index * 100) + index2}>{product.productName}</div>)
+
+        ))
+    );
+}
+
+export default function Category({category}) {
+
+    return (
+        <ListItems type={category}/>
+    );
+}
+*/
+
+/*
+
+*/
