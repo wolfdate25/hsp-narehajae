97e678f8aaf960cfc26a4ce57693d8e22748e6c4
yuseon-Lim <lys4486@gmail.com>
2021-02-15 17:07:46 +0900

    delete comment

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -52,27 +52,3 @@ class Category extends React.Component {
 }
 
 export default Category;
-
-/*
-import React from "react";
-import {organizations} from "./Products";
-
-
-function ListItems({type}) {
-    return (
-        organizations.map((company, index) => (
-            company.products.filter(product =>
-                product.category.includes(type)
-            ).map((product, index2) => <div key={(index * 100) + index2}>{product.productName}</div>)
-
-        ))
-    );
-}
-
-export default function Category({category}) {
-
-    return (
-        <ListItems type={category}/>
-    );
-}
-*/
